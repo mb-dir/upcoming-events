aee3a62380505513d0766bd5b5495915e5a48217
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-05-12 21:31:38 +0200

    Create a method
    This method is responsible for saving information about upcoming events
    in localstore

--- assets/scripts/components/SaveStore.js
+++ assets/scripts/components/SaveStore.js
@@ -2,6 +2,19 @@ class SaveStore{
     constructor(eventName, eventDate){
         this.eventName = eventName;
         this.eventDate = eventDate;
+        this.saveEvent();
+    }
+    saveEvent(){
+        //If array called "eventsToSave" does not exist in localstore create an empty array, if exist use it
+        //On the first use there is no "eventsToSave" in localstore, so we need to create this array
+        //In each subsquent use there is "eventsToSave" in localstore, so we need not to create an empty array, we need push another one object(event) to an existing array 
+        const eventsToSave = JSON.parse(window.localStorage.getItem('events')) ? JSON.parse(window.localStorage.getItem('events')) : [];
+        const event = {
+            name: this.eventName,
+            date: this.eventDate,
+        }
+        eventsToSave.push(event);
+        window.localStorage.setItem('events', JSON.stringify(eventsToSave));
     }
 }
 
