0a2003998a4e00c3de89a031eba297a62d8103db
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-05-12 20:04:34 +0200

    Create necessary variables

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,4 +2,8 @@ const addEventForm = document.querySelector('#addEventForm');
 
 addEventForm.addEventListener('submit', (e)=>{
     e.preventDefault();
+
+    //variables
+    const eventName = document.querySelector('#eventContents').value;
+    const eventDate = document.querySelector('#eventDate').value;
 });
