fd3c3913df7a8571801d0c2ed17aee41d9b76915
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-05-13 21:41:26 +0200

    Work on method
    Create drawList method which will be responsible for draw list of events
    based on events array from localstore. In this moment this method saves
    and logs the array.

--- assets/scripts/components/DrawList.js
+++ assets/scripts/components/DrawList.js
@@ -2,5 +2,10 @@ class DrawList{
     constructor(nameEventsArray){
         //this property stores name of events array which is in localstore 
         this.nameEventsArray = nameEventsArray;
+        this.drawList();
+    }
+    drawList(){
+        const eventsArray = JSON.parse(window.localStorage.getItem(this.nameEventsArray))
+        console.log(eventsArray)
     }
 }
