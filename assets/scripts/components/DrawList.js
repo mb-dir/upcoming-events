05eb98025265acf3f4e9f15fb180312eab5e79a4
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-05-14 20:57:08 +0200

    Create new method
    This method is responsible for creating list element based on name and
    date from localstorage.
    In drawList I use it to check if it works - and yes it works

--- assets/scripts/components/DrawList.js
+++ assets/scripts/components/DrawList.js
@@ -8,8 +8,35 @@ class DrawList{
         const eventsArray = JSON.parse(window.localStorage.getItem(this.nameEventsArray))
         eventsArray.forEach(ev => {
             const {date, name} = ev
-            console.log(date, name)
+            console.log(this.createListItem(name, date))
         });
+    }
+    createListItem(name, date){
+        const listItem = document.createElement('li');
+        listItem.classList.add('list__item');
+
+        const dataContainer = document.createElement('div');
+        dataContainer.classList.add('dataContainer');
+
+        const list__eventContents = document.createElement('div');
+        list__eventContents.classList.add('list__eventContents');
+        list__eventContents.textContent = name;
+
+        const list__eventDate = document.createElement('div');
+        list__eventDate.classList.add('list__eventDate');
+        list__eventDate.textContent = date;
+
+        const btnDelete = document.createElement('button');
+        btnDelete.className = "btn btn--deleteEvent";
+        
+        //combining components of list item
+
+        dataContainer.appendChild(list__eventContents);
+        dataContainer.appendChild(list__eventDate);
+        listItem.appendChild(dataContainer);
+        listItem.appendChild(btnDelete);
+
+        return listItem;
     }
 }
 export default DrawList;
