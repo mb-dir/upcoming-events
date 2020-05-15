e0eac0bb6e592169d770c87b217b5eb44d0b800f
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-05-15 20:41:22 +0200

    Create class

---
+++ assets/scripts/components/EventsHandling.js
@@ -0,0 +1,9 @@
+class EventsHandling{
+    constructor(eventName, eventDate, referenceToList) {
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.referenceToList = referenceToList;
+    }
+}
+
+export default EventsHandling;
