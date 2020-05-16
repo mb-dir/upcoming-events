672e5956516e98767b517706da49de25a3eaed26
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-05-16 21:05:56 +0200

    Create a new class file
    I want to delegate the creation of list items and their
    handling(deleting) to another class

---
+++ assets/scripts/components/CreateListItem.js
@@ -0,0 +1,9 @@
+class CreateListItem {
+    constructor(eventName, eventDate, referenceToList) {
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.referenceToList = referenceToList;
+    }
+}
+
+export default CreateListItem;
