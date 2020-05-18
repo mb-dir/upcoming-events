2cea8a81dfa19141fecc8832e54276d37a286190
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-05-18 20:42:22 +0200

    Create a new class
    This class will be responsible for deleting event from localstorage when
    user deletes the event from list

---
+++ assets/scripts/components/DeleteFromStore.js
@@ -0,0 +1,9 @@
+class DeleteFromStore{
+    constructor(name, date, nameEventsArray) {
+        this.name = name;
+        this.date = date;
+        this.nameEventsArray = nameEventsArray;
+    }
+}
+
+export default DeleteFromStore;
