b255ed67a0a8d427c80ac7820e6f04b990e7a180
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-05-30 16:41:16 +0200

    Create basic class template

---
+++ assets/scripts/components/validator/DateValidator.js
@@ -0,0 +1,8 @@
+class DateValidator{
+    constructor(eventDateInput, eventDate){
+        this.eventDateInput = eventDateInput;
+        this.eventDate = eventDate;
+    }
+}
+
+export default DateValidator;
