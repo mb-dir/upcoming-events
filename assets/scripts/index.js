c66775aadafa06499be282d3bc5b0af2d81fef60
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-05-30 17:26:34 +0200

    Import the class
    I import the class and use the method

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,7 @@
 import SaveStore from './components/upcoming_events/SaveStore.js';
 import DrawList from './components/upcoming_events/DrawList.js';
 import EventsHandling from './components/upcoming_events/EventsHandling.js';
+import DateValidator from './components/validator/DateValidator.js';
 
 const addEventForm = document.querySelector('#addEventForm');
 const eventsList = document.querySelector('#eventsList');
@@ -14,6 +15,7 @@ addEventForm.addEventListener('submit', (e)=>{
     const eventName = document.querySelector('#eventContents').value;
     const eventDate = eventDateInput.value;
 
+    const dateValidator = new DateValidator(eventDateInput, eventDate);
     const saveEvent = new SaveStore(eventName, eventDate);
     const eventsHandling = new EventsHandling(eventName, eventDate, eventsList);
     eventsHandling.dynamicallyAddEvent();
