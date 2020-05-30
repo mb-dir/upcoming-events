5f66ae28ef7e75b35739a9e34687103483f68648
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-05-30 17:17:37 +0200

    Add a new method
    This method is intented to check the date. For the method to work I had
    to change the date notation - based on the eventDate, which is a string,
    I create a date object, which respresents the date entered by the user.

--- assets/scripts/components/validator/DateValidator.js
+++ assets/scripts/components/validator/DateValidator.js
@@ -1,7 +1,14 @@
 class DateValidator{
     constructor(eventDateInput, eventDate){
         this.eventDateInput = eventDateInput;
-        this.eventDate = eventDate;
+        this.eventDate = new Date(eventDate);
+        this.validDate();
+    }
+    validDate(){
+        const currentDate = new Date();
+        if(currentDate > this.eventDate){
+            throw "Incorrect date!"
+        }
     }
 }
 
