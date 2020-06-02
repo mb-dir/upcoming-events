08592bf288053f9faef83bfdffcae47cddf8f8ae
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-06-02 17:52:08 +0200

    Add a new method
    This method will be responsible for sorting events by date. Now it is just
    iterates over events array from localstorage

--- assets/scripts/components/sorter/DateSorter.js
+++ assets/scripts/components/sorter/DateSorter.js
@@ -1,6 +1,13 @@
 class DateSorter{
     constructor(arrayEventsName){
         this.arrayEventsName = arrayEventsName;
+        this.sort();
+    }
+    sort(){
+        const eventsToSort = JSON.parse(window.localStorage.getItem(this.arrayEventsName));
+        for(const item of eventsToSort){
+            console.log(item.date)
+        }
     }
 }
 
