dc721494c24019b7d4c293673f53b1e0884658c3
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-06-08 23:00:17 +0200

    Add a new method
    This method is responsible for checking if date of event is
    outdated, if it is this method will delete it.

--- assets/scripts/components/upcoming_events/DeleteOldEvents.js
+++ assets/scripts/components/upcoming_events/DeleteOldEvents.js
@@ -1,6 +1,20 @@
 class DeleteOldEvents{
     constructor(nameEventsArray) {
         this.nameEventsArray = nameEventsArray;
+        this.checkEventsDate();
+    }
+    checkEventsDate(){
+        const eventsArray = JSON.parse(window.localStorage.getItem(this.nameEventsArray));
+        const nowDate = new Date();
+        const yesterday = nowDate.setDate(nowDate.getDate() - 1)
+
+        eventsArray.forEach((ev, evIndex, evArr) => {
+            //cuz I do not want to delete events which happened that day
+            if (new Date(ev.date) < yesterday) {
+                evArr.splice(evIndex, 1);
+                window.localStorage.setItem(this.nameEventsArray, JSON.stringify(evArr));
+            }
+        });
     }
 }
 
