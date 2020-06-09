073015aa5e9c410f552305c7238d8167a9421ef9
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-06-09 17:22:27 +0200

    Change a method
    I add a condition which checks if the event is outdated, if it is then
    it will not be drawing.

--- assets/scripts/components/upcoming_events/DrawList.js
+++ assets/scripts/components/upcoming_events/DrawList.js
@@ -10,12 +10,19 @@ class DrawList{
     //Methods responsible for drawing events list based on localstorage
     drawList(){
         const eventsArray = JSON.parse(window.localStorage.getItem(this.nameEventsArray))
+        const nowDate = new Date();
+        const yesterday = nowDate.setDate(nowDate.getDate() - 1)
         if (eventsArray !== null) {
             eventsArray.forEach(ev => {
                 const {date, name} = ev;
-                const listItemCreator = new CreateListItem(name, date, this.referenceToList);
-                const listItem = listItemCreator.createListItem();
-                this.referenceToList.appendChild(listItem);
+                //If the event is outdated it will not be draw
+                if (new Date(ev.date) < yesterday) {
+                    return;
+                }else{
+                    const listItemCreator = new CreateListItem(name, date, this.referenceToList);
+                    const listItem = listItemCreator.createListItem();
+                    this.referenceToList.appendChild(listItem);
+                }
             });
         }
     }
