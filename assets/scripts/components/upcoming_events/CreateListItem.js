93c41e8be01e775e75a8fedad397e972a7d75451
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-06-11 16:12:49 +0200

    Change in method structure
    If event is close this method will color date of event on red

--- assets/scripts/components/upcoming_events/CreateListItem.js
+++ assets/scripts/components/upcoming_events/CreateListItem.js
@@ -21,6 +21,9 @@ class CreateListItem {
         const list__eventDate = document.createElement('div');
         list__eventDate.classList.add('list__eventDate');
         list__eventDate.textContent = `Data wydarzenia: ${this.eventDate}`;
+        if(this.isCloseEvent){
+            list__eventDate.style.color = "#ff0000";
+        }
 
         const btnDelete = document.createElement('button');
         btnDelete.className = "btn btn--deleteEvent";
