9fe1dc2eb4922a0a4fcc882eeec9735059d317b5
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-08-23 21:56:22 +0200

    Create a new method
    This new method is responsible for handling clicks in the btn(wchih
    changed color mode).
    Now this method only log a short information(I wanted to see if it works), now I am going to write some styles

--- assets/scripts/components/darkmode_handling/Darkmode.js
+++ assets/scripts/components/darkmode_handling/Darkmode.js
@@ -4,6 +4,14 @@ class DarkMode {
         this.bodyDarkModeClassName = bodyDarkModeClassName;
         this.btnModeChange = btnModeChange;
         this.elementsToChange = elementsToChange;//list of element to which the suffix will be added
+
+        this.buttonHandling();
+    }
+
+    buttonHandling(){
+        this.btnModeChange.addEventListener('click', ()=>{
+            console.log("It works, you can create styles for dark mode")
+        })
     }
 }
 
