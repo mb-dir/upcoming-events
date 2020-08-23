44291a20cc2e631f5b151e994c86ff353dd49ce6
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-08-23 21:37:46 +0200

    Add basic properties to constructor
    darkModeSuffix = suffix which will be added to class in dark mode
    elementsToChange = list of element to which the suffix will be addedt
    status

--- assets/scripts/components/darkmode_handling/Darkmode.js
+++ assets/scripts/components/darkmode_handling/Darkmode.js
@@ -1,5 +1,9 @@
 class DarkMode {
-    constructor() {
+    constructor(darkModeSuffix, bodyDarkModeClassName, btnModeChange, ...elementsToChange) {
+        this.darkModeSuffix = darkModeSuffix;//suffix which will be added to class in dark mode
+        this.bodyDarkModeClassName = bodyDarkModeClassName;
+        this.btnModeChange = btnModeChange;
+        this.elementsToChange = elementsToChange;//list of element to which the suffix will be added
     }
 }
 
