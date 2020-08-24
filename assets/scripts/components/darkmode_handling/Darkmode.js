019c967575a3d2357108e2f767079d6f11f99857
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-08-24 16:13:32 +0200

    Delete unnecessary properties
    Thanks to the changes from last commit these properties are completly
    unnecessary

--- assets/scripts/components/darkmode_handling/Darkmode.js
+++ assets/scripts/components/darkmode_handling/Darkmode.js
@@ -1,9 +1,7 @@
 class DarkMode {
-    constructor(darkModeSuffix, bodyDarkModeClassName, btnModeChange, ...elementsToChange) {
-        this.darkModeSuffix = darkModeSuffix;//suffix which will be added to class in dark mode
+    constructor(bodyDarkModeClassName, btnModeChange) {
         this.bodyDarkModeClassName = bodyDarkModeClassName;
         this.btnModeChange = btnModeChange;
-        this.elementsToChange = elementsToChange;//list of element to which the suffix will be added
 
         this.buttonHandling();
     }
