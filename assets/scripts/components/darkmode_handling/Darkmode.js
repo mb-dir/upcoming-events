4fe660561713a65df14b6347ab04b7a500ff40a5
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-06-02 22:50:24 +0200

    My mistake correction
    getItem returnes string, I have forgotten about that, and there was a
    samll issue, but now it work properly

--- assets/scripts/components/darkmode_handling/Darkmode.js
+++ assets/scripts/components/darkmode_handling/Darkmode.js
@@ -10,7 +10,7 @@ class DarkMode {
         this.btnModeChange.addEventListener('click', ()=>{
             document.body.classList.toggle(this.bodyDarkModeClassName);
             //When user clicks the button, check if there is "darkMode" in sotrage, if it isn't(condition returns false but "!" makes it true) set darkMode to true else set false
-            if (!window.localStorage.getItem("darkMode")){
+            if (window.localStorage.getItem("darkMode") !== "true"){
                 window.localStorage.setItem("darkMode", true);
             }else{
                 window.localStorage.setItem("darkMode", false);
